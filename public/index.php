4b044152d476ec92b68af08cf918f506895cdc61
Valeriy <95502737+VaLeraGav@users.noreply.github.com>
2022-11-08 21:47:14 +0300

    add page view

--- public/index.php
+++ public/index.php
@@ -6,18 +6,28 @@ use DI\Container;
 use Slim\Factory\AppFactory;
 
 $container = new Container();
+
 $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
+$container->set('flash', function (){
+    return new \Slim\Flash\Messages();
+});
+
 
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
+
     return $this->get('renderer')->render($response, 'index.phtml');
-})->setName('main');
+})->setName('index');
 
 
+$app->get('/urls', function ($request, $response) {
+
+    return $this->get('renderer')->render($response, 'show.phtml');
+})->setName('urls');
 
 
 
